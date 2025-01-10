7ad2c8ffab56647cc05962906e8cf0a571ced072
muhammad-sayem <shahrulislamsayem@gmail.com>
2025-01-10 22:09:50 +0600

    JWT Added

--- index.js
+++ index.js
@@ -8,31 +8,32 @@ const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const corsOptions = {
+    origin: ['http://localhost:5173',
+        'http://localhost:5174'
+    ],
+    credentials: true,
+    optionalSuccessStatus: 200,
+}
+
 // Middleware //
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-// const logger = (req, res, next) => {
-//     console.log("inside the logger");
-//     next();
-// }
-
-// const verifyToken = (req, res, next) => {
-//     const token = req?.cookies?.token;
-
-//     if(!token){
-//         return res.status(401).send({message: "Unauthorized access"})
-//     }
-
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//         if(err){
-//             res.status(401).send({message: "Unauthorized acces"})
-//         }
-//         req.user = decoded;
-//         next();
-//     })
-// }
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+    if(!token){
+        return res.status(401).send({message: "Unauthorized access!!"})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({message: "Unauthorized access!!"})
+        }
+        req.user = decoded;
+    })
+    next();
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crzce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -55,20 +56,30 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
-        // Auth Related API's //
-        // app.post('/jwt', async(req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        //         expiresIn: '5h'
-        //     })
+        // Generate JWT //
+        app.post('/jwt', async (req, res) => {
+            const email = req.body;
+            console.log(email);
+            // Create token //
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' });
+            // console.log(token);
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            })
+            .send({success: true})
+        });
 
-        //     res
-        //     .cookie('token', token, {
-        //         httpOnly: true,
-        //         secure: false
-        //     })
-        //     .send({success: true})
-        // })
+         // Clear token from cookie in browser || Logout //
+         app.get('/logout', async(req, res) => {
+            res.clearCookie('token', {
+                maxAge: 0,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            })
+            .send({status: true})
+        })
 
         const artifactsCollection = client.db('historicalArtifacts').collection('artifacts');
         const likedArtifactsCollection = client.db('historicalArtifacts').collection('likedArtifacts');
@@ -82,12 +93,16 @@ async function run() {
 
         // Get All artifacts from artifactsCollection //
         app.get('/artifacts', async(req, res) => {
-            const result = await artifactsCollection.find().toArray();
+            const result = await artifactsCollection
+            .find()
+            .sort({likes: -1})
+            .limit(6)
+            .toArray();
             res.send(result);
         });
 
         // Get a specific artifact form artifactsCollection //
-        app.get('/artifact/:id', async(req, res) => {
+        app.get('/artifact/:id', verifyToken, async(req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
             const result = await artifactsCollection.findOne(query);
@@ -95,7 +110,7 @@ async function run() {
         });
 
         // Delete a specific artifact from artifactsCollection //
-        app.delete('/artifact/:id', async(req, res) => {
+        app.delete('/artifact/:id', verifyToken, async(req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
             const result = await artifactsCollection.deleteOne(query);
@@ -103,7 +118,7 @@ async function run() {
         })
 
         // Get specific artifacts from artifactsCollection by an email //
-        app.get('/artifacts/:email', async(req, res) => {
+        app.get('/artifacts/:email', verifyToken, async(req, res) => {
             const email = req.params.email;
             const query = {adderEmail: email};
             const result = await artifactsCollection.find(query).toArray();
@@ -151,7 +166,7 @@ async function run() {
         });
 
         // Get liked artifacts for a specific email //
-        app.get('/liked-artifacts/:email', async(req, res) => {
+        app.get('/liked-artifacts/:email', verifyToken, async(req, res) => {
             const email = req.params.email;
             const query = {likedEmail: email};
             const result = await likedArtifactsCollection.find(query).toArray();
@@ -159,7 +174,7 @@ async function run() {
         });
         
         // Update an artifact //
-        app.put('/update/:id', async(req, res) => {
+        app.put('/update/:id', verifyToken, async(req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
             const artifactData = req.body;
@@ -186,8 +201,6 @@ async function run() {
             const result = await artifactsCollection.find(query).toArray();
             res.send(result);
         })
-
-        
 
     } finally {
         // Ensures that the client will close when you finish/error
