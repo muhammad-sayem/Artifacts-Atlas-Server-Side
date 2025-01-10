1bc6301b649f500c0d7495fc8ce72ea93aae3952
muhammad-sayem <shahrulislamsayem@gmail.com>
2025-01-11 02:07:32 +0600

    vercel added

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const corsOptions = {
     origin: ['http://localhost:5173',
-        'http://localhost:5174'
+        'http://localhost:5174',
+        'https://historical-artifacts-tracker-server-three.vercel.app'
+
     ],
     credentials: true,
     optionalSuccessStatus: 200,
@@ -50,9 +52,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
